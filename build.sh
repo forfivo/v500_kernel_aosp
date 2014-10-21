7f7041bd19f2e6f2e59a31aa54873d332b42e7eb
dr.beat <xgecco@gmail.com>
2014-10-21 18:29:50 +0200

    build: back to linaro

--- build.sh
+++ build.sh
@@ -25,8 +25,7 @@ echo -e "${restore}"
 export LOCALVERSION="-"`echo $KVER`
 export ARCH=arm
 export SUBARCH=arm
-#export CROSS_COMPILE="$HOME/android/a15-linaro-4.9.2/bin/arm-cortex_a15-linux-gnueabihf-"
-export CROSS_COMPILE="$HOME/android/arm-linux-4.8/bin/arm-eabi-"
+export CROSS_COMPILE="$HOME/android/a15-linaro-4.9.2/bin/arm-cortex_a15-linux-gnueabihf-"
 
 echo -e "${green}"
 echo ">>> build zImage"
