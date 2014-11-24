e955184a5bc4ab0c4e0c7bfe8c1b608bed5e0221
forfivo <forfivo@gmail.com>
2014-11-24 03:19:24 -0200

    LOW MEM KILLER: restore stock array sets.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -50,34 +50,27 @@ static int lowmem_adj[6] = {
 	4,
 	9,
 	12,
-	15,
 };
-static int lowmem_adj_size = 6;
+static int lowmem_adj_size = 4;
 static int lowmem_minfree[6] = {
 	3 * 512,	/* 6MB */
 	2 * 1024,	/* 8MB */
 	4 * 1024,	/* 16MB */
-	12 * 1024,	/* 49MB */
 	16 * 1024,	/* 64MB */
-	20 * 1024,	/* 128MB */
 };
 static int lowmem_minfree_screen_off[6] = {
 	3 * 512,	/* 6MB */
 	2 * 1024,	/* 8MB */
 	4 * 1024,	/* 16MB */
-	12 * 1024,	/* 49MB */
 	16 * 1024,	/* 64MB */
-	20 * 1024,	/* 128MB */
 };
 static int lowmem_minfree_screen_on[6] = {
 	3 * 512,	/* 6MB */
 	2 * 1024,	/* 8MB */
 	4 * 1024,	/* 16MB */
-	12 * 1024,	/* 49MB */
 	16 * 1024,	/* 64MB */
-	20 * 1024,	/* 128MB */
 };
-static int lowmem_minfree_size = 6;
+static int lowmem_minfree_size = 4;
 static int lmk_fast_run = 1;
 
 static unsigned long lowmem_deathpending_timeout;
